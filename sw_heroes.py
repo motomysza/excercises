d6c7bf4435ed8b9d9175fddd1a43753a68193e8f
motomysza <32395298+motomysza@users.noreply.github.com>
2017-11-13 18:43:23 +0100

    come from

---
+++ sw_heroes.py
@@ -0,0 +1,20 @@
+import json
+with open("py1.2.json") as file:
+    new_data = json.load(file)
+heros = open("sw_all_heros", "w")
+
+for element in new_data:
+    single_sentence = element["name"]
+    if element["mass"] != "unknown":
+        single_sentence += " waży " + element["mass"] + " kg"
+    if element["gender"] == "female":
+        single_sentence += " jest kobietą"
+    elif element["gender"] == "male":
+        single_sentence += " jest mężczyzną"
+    if element["homeworld"] != "unknown":
+        single_sentence += " i pochodzi z " + element["homeworld"]
+    single_sentence += ".\n"
+    heros.writelines(single_sentence)
+
+file.close()
+heros.close()
